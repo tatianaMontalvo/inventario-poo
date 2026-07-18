9c50e71fc080546b2a4be417e69aa073c3d8c5e2
Tatiana Montalvo <drivas.tatiana@gmail.com>
2026-07-18 15:05:56 -0600

    clase producto

--- src/sistema_inventario.py
+++ src/sistema_inventario.py
@@ -1,1 +1,49 @@
-prueba
+class Producto:
+    """
+    Clase que contiene los atributos y metodos disponibles del producto
+    """
+    nombre =""
+    precio = 0.0
+    cantidad = 0
+
+    def __init__(self, nombre,precio,cantidad):
+        """
+        Metodo constructor que inicializa los atributos
+        """
+        if precio < 0:
+            raise ValueError("El precio debe ser mayor o igual que cero.")
+        if not nombre or not nombre.strip():
+            raise ValueError("El nombre no puede estar vacío ni contener solo espacios.")
+        if cantidad < 0:
+            raise ValueError("La cantidad debe ser mayor o igual que cero.")
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def actualizar_precio(self, nuevo_precio):
+        """
+        funcion que sirve para modificar el precio validando que sea mayor o igual que cero
+        """
+        if nuevo_precio >= 0:
+            precio = nuevo_precio
+
+    def actualizar_cantidad(self,nueva_cantidad): 
+        """
+        funcion que sirve para modificar la cantidad validando que sea mayor o igual a cero
+        """
+        if nueva_cantidad >= 0:
+            cantidad = nueva_cantidad
+
+    def calcular_valor_total(self):
+        """
+        funcion que sirve para devolver el valor total (precio × cantidad)
+        """
+        valor_total = precio * cantidad
+        
+    def __str__(self):
+        """
+        funcion que sirve para mostrar la información del producto de forma legible
+        """
+        return ( f"📦 [{self.nombre.upper()}]\n"
+            f" ├─ Precio: ${self.precio:,.2f}\n"
+            f" └─ Cantidad:  {self.cantidad} unidades")
